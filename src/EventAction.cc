3d155f69e62919c2fbb0eaf976f59e915b69d958
John Murphy <jxm1605@student.bham.ac.uk>
2026-05-18 17:36:17 +0100

    completed eventaction.cc logic scoring information about energy deposition during event and recoil nuclei contributing

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -6,6 +6,7 @@
 #include "RunAction.hh"
 #include "G4AnalysisManager.hh"
 #include "globals.hh"
+#include "G4Event.hh"
 
 namespace AmBeStack
 {
@@ -23,19 +24,77 @@ namespace AmBeStack
     // first member function
     // actually define the BeginOfEventAction function
     // returntype class::functionname(args)
-    // called at each start of each event in the simulation
+    // called at start of each event in the simulation
     // acts as a hard RESET
-    void EventAction::BeginOfEventAction(const G4Event*)
+    void EventAction::BeginOfEventAction(const G4Event* event)
     {
         fEdep = 0.; // reset total event energy back to 0
 
         // wipe the maps containing the information about the interaction
-        // of the neutron
-        recoilNameMap.clear();
-        recoilInitialKEMap.clear();
-        recoilFinalKEMap.clear();
+        // of the neutron (event)
+        eventIDMap.clear(); // new event ID
+        totEventEdepMap.clear(); // new tot energy deposited from the event
+        recoilNameMap.clear(); // new recoil name
+        recoilEdepMap.clear(); // new energy deposited by this recoil from event
     }
 
-    void 
+    // second member function
+    // actually define EndOfEventAction function
+    // returntype class::functionname(args)
+    // called at end of each event in simulation
+    // may contain information about multiple particles
+    // all stemming from same event
+    // need to go through and process each of these
+    void EventAction::EndOfEventAction(const G4Event* event)
+    {
+        // make an analysis manager instance
+        // allows us to write from the map values to the output file
+        G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+
+        // get the eventID
+        G4int eventID = event->GetEventID();
+
+        // if we have generated no recoil nuclei within the c6d6
+        // recoilEdepMap will still be cleared from the BeginOfEventAction
+        // since the if (particle->GetBaryonNumber() >= 1) loop in
+        // steppingaction.cc won't have ran
+        // still need to record data though as event still occurred
+        if (recoilEdepMap.empty())
+        {
+            analysisManager->FillNTupleIColumn(0, eventID); // column 0, unique event ID (integer)
+            analysisManager->FillNTupleDColumn(1, fEdep); // column 1, total event energy deposition
+            analysisManager->FillNTupleSColumn(2, "No_recoil"); // column 2, would've been recoil nuclei name
+            analysisManager->FillNTupleDColumn(3, 0.0); // column 3, recoil energy (none here)
+            // move to new row
+            analysisManager->AddNTupleRow();
+        }
+        // otherwise, recoil nuclei must've been tracked
+        else
+        {
+            // go through each of them and add extract
+            // maps have following structure:
+            // map[Key, Value]
+            // for each pair in recoilEdepMap: {...}
+            // this is because we may have multiple pairs
+            // [trackID, recoilEdep] for multiple recoils
+            // associated with the same event
+            // auto sorts typing
+            // const means things can't be modified in the map
+            // & means reference (look directly at data inside map, don't copy)
+            for (auto const& [trackID, recoilEdep] : recoilEdepMap)
+            {
+                // get corresponding recoilNameMap value
+                G4String recoilName = recoilNameMap[trackID];
+                // write to analysis manager
+                analysisManager->FillNTupleIColumn(0, eventID);
+                analysisManager->FillNTupleDColumn(1, fEdep);
+                analysisManager->FillNTupleSColumn(2, recoilName);
+                analysisManager->FillNTupleDColumn(3, recoilEdep);
+
+                // move to new row for either new event or new recoil nuclei
+                analysisManager->AddNTupleRow();
+            }
+        }
+    }
 }
 
